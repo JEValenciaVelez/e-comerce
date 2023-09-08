21ef4ed5ccefbca7a5227cefcd42a0088f8265be
unknown <juancho50301@gmail.com>
2023-09-08 16:11:48 -0500

    consumo a la api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,33 @@
+import {useState, useEffect} from 'react'
+
+import { Card } from "../../components/Card"
 import { Layout } from "../../components/Layout"
 import { NavBar } from "../../components/NavBar"
 
 
 export const Home = () => {
 
+    const [items, setItems] = useState(null);
+
+    useEffect(()=>{
+        fetch('https://api.escuelajs.co/api/v1/products')
+        .then(response => response.json())
+        .then(data => setItems(data))
+        .catch(error => console.log(error));
+    },[]);
+
+    console.log('Estos son los items de la api ->',items)
+
     return (
         <Layout>
             <NavBar />
+            <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+            {
+                items?.map((item)=>(
+                    <Card key={item.id} data={item} />
+                ))
+            }
+            </div>
         </Layout>
     )
 }
