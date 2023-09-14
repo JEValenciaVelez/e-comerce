dc20aeaa06236812af0b880bb0db454d1f02621f
unknown <juancho50301@gmail.com>
2023-09-14 12:13:48 -0500

    oders-page

--- src/pages/MyOrder/index.jsx
+++ src/pages/MyOrder/index.jsx
@@ -9,12 +9,12 @@ import OrderCard from '../../components/OrderCard';
 export const MyOrder = () => {
 
     const context = useContext(contextShoppingCart);
-    const lastOrder = context.order?.slice(-1)[0].products ;
+    const lastOrder = context.order?.slice(-1)[0].products || [];
 
     return (
         <Layout>
             <h2>My Order Works</h2>
-            <div className="px-6 overflow-scroll flex-1 ">
+            <div className=" flex flex-col w-80 ">
 
             {
                 lastOrder.map((product) => (
--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -1,11 +1,18 @@
+//componente de lista de ordenes
+import { useContext } from 'react'
+
 import { Layout } from "../../components/Layout"
+import OrdersCard from "../../components/OredersCard"
 
 
 export const MyOrders = () => {
 
+    const context = useContext(contextShoppingCart);
+
     return (
         <Layout>
             <h2>My orders Works</h2>
+            <OrdersCard />
         </Layout>
     )
 }
